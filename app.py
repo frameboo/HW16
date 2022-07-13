4ba5baf1fb4a610fc1bd5c503b7c142b2297ec39
frame <frame.boo@gmail.com>
2022-07-13 23:02:09 +0300

    1

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+import json
+
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import relationship
+
 import utils
-import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db?charset=utf-8'
